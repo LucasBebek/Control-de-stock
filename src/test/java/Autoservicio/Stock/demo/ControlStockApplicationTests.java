a59cdc611c0172233e4e91399b3a42e80e3dc8db
LucasBebek <bebeklucas7@gmail.com>
2023-11-27 14:40:28 -0300

    primer commit

---
+++ src/test/java/Autoservicio/Stock/demo/ControlStockApplicationTests.java
@@ -0,0 +1,13 @@
+package Autoservicio.Stock.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
